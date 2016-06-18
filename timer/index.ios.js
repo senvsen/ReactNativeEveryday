69e80a6f6cc3acd1f20c1aec8acbf25d1d368af3
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-06-18 14:22:51 -0400

    Add complete version

---
+++ timer/index.ios.js
@@ -0,0 +1,81 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class timer extends Component {
+  render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.timerDisplayLayout}>
+            <View style={styles.timerCircle}>
+              <Text style={styles.mainLabel}>
+                00:00
+              </Text>
+
+              <Text style={styles.subLabel}>
+                00
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.controlsLayout}>
+          </View>
+        </View>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+
+  timerCircle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 200,
+    height: 200,
+    borderColor: '#fff',
+    borderWidth: 5,
+    borderRadius: 100,
+  },
+
+  mainLabel: {
+    color: '#fff',
+    fontSize: 50
+  },
+
+  subLabel: {
+    color: '#fff',
+    fontSize: 20,
+    marginTop: 15,
+    marginLeft: 1
+  },
+
+  // flexDirection set default to column.
+  timerDisplayLayout: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255,0,0,0.3)',
+  },
+
+  controlsLayout: {
+    flex: 1.5,
+    backgroundColor: 'rgba(0,255,0,0.3)',
+  },
+});
+
+AppRegistry.registerComponent('timer', () => timer);
