6d383ca075810f4cb43d7048d56f21fa42ca219a
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-06-19 16:47:01 -0400

    Update api to fetch sentiment result

--- Day2Sentiment/index.ios.js
+++ Day2Sentiment/index.ios.js
@@ -9,45 +9,105 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Image
 } from 'react-native';
+const Forecast = require('./Forecast');
 
 class Day2Sentiment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      sentiment: 'happy'
+    };
+  };
+
+  handleTextChange(event) {
+    var searchText = event.nativeEvent.text;
+    this.setState({searchText: searchText});
+    fetch('http://www.airloft.org/api/getSentimentTextMulti?text='
+      + searchText)
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        this.setState({
+          sentiment: responseJSON.results[0].sentiment
+        });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  };
+
   render() {
+    var content = null;
+    if (this.state.sentiment !== null) {
+      content = <Forecast main={this.state.sentiment} />;
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+          <View style={styles.overlay}>
+           <View style={styles.row}>
+             <Text style={styles.mainText}>
+               Current sentiment for
+             </Text>
+             <View style={styles.zipContainer}>
+               <TextInput
+                 style={[styles.zipCode, styles.mainText]}
+                 onSubmitEditing={this.handleTextChange.bind(this)}/>
+             </View>
+           </View>
+           {content}
+         </View>
       </View>
     );
   }
+
 }
+
+const baseFontSize = 16;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 30
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30
+  },
+  zipContainer: {
+    flex: 1,
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: {
+    width: 50,
+    height: baseFontSize,
+  },
+  mainText: {
+    flex: 1,
+    fontSize: baseFontSize,
+    color: '#FFFFFF'
+  }
 });
 
 AppRegistry.registerComponent('Day2Sentiment', () => Day2Sentiment);
