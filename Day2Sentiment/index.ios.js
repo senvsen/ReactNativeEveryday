14178ed6771e2859c3a1123641bcd69506ec2c7b
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-06-21 16:53:09 -0400

    Add image touching to next

--- Day2Sentiment/index.ios.js
+++ Day2Sentiment/index.ios.js
@@ -12,23 +12,29 @@ import {
   Text,
   View,
   TextInput,
-  Image
+  Image,
+  TouchableOpacity
 } from 'react-native';
 const Forecast = require('./Forecast');
 class Day2Sentiment extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isStart: false,
       searchText: '',
       sentiment: '',
-      gif: 'http://ww1.sinaimg.cn/large/72f96cbagw1f52lv09lzgj211o0vstda'
+      gif: 'http://ww1.sinaimg.cn/large/72f96cbagw1f52lv09lzgj211o0vstda',
+      notice: ''
     };
   };
 
   handleTextChange(text) {
     var searchText = text;
     var _this = this;
-    this.setState({searchText: searchText});
+    this.setState({
+      isStart: true,
+      searchText: searchText
+    });
     fetch('http://www.airloft.org/api/getSentimentTextMulti?text='
       + searchText)
       .then((response) => response.json())
@@ -38,19 +44,33 @@ class Day2Sentiment extends Component {
           sentiment: sentiment
         });
         var firstSenti = sentiment[0].split("/")[0];
-        var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + firstSenti;
-        fetch(url)
-        .then((imgresponse) => imgresponse.json())
-        .then(function(imgresponseJSON){
-          console.log(imgresponseJSON.data.image_url);
-          _this.setState({
-            gif: imgresponseJSON.data.image_url
-          });
-        })
+        _this.handleGifFetch(firstSenti, _this);
       })
       .catch((error) => {
         console.warn(error);
       });
+  };
+
+  /**
+   * _this: points to current component.
+   */
+  handleGifFetch(text, _this){
+    var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + text;
+    fetch(url)
+    .then((imgresponse) => imgresponse.json())
+    .then(function(imgresponseJSON){
+      console.log(imgresponseJSON.data.image_url);
+      _this.setState({
+        gif: imgresponseJSON.data.image_url
+      });
+    })
+  };
+
+  onPress(event){
+    if(this.state.isStart == true){
+      var searchText = this.state.sentiment[0].split("/")[0];
+      this.handleGifFetch(searchText, this);
+    }
   };
 
   render() {
@@ -76,10 +96,13 @@ class Day2Sentiment extends Component {
            </View>
            {content}
          </View>
-         <Image style={[styles.mainGif, {
-           width: width
-         }]} source={{uri: this.state.gif}}>
-         </Image>
+         <TouchableOpacity style={styles.mainGif} onPress={this.onPress.bind(this)}>
+           <Image style={[styles.mainGif, {
+             width: width
+           }]} source={{uri: this.state.gif}}>
+           </Image>
+         </TouchableOpacity>
+
       </View>
     );
   }
