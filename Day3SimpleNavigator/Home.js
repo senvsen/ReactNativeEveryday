270c38b865017af7d7b3ab363a772dd3af2425dc
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-06-25 16:03:31 -0400

    Finish mainpage styling, added listview page

--- Day3SimpleNavigator/Home.js
+++ Day3SimpleNavigator/Home.js
@@ -2,24 +2,56 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
 
 class Home extends Component {
+
+  _handleListBook() {
+    console.log("pressed!");
+  };
+
   render() {
     return (
-      <View style={styles.home}>
-        <Text>
-          hello world!
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.topBlock} />
+        <View style={styles.bottomBlock}>
+          <TouchableHighlight style={styles.button}
+          onPress={this._handleListBook}
+          underlayColor="#99d9f4">
+            <Text style={styles.buttonText}>
+              List books
+            </Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  home: {
+  container: {
     flex: 1
+  },
+  topBlock: {
+    flex: 2
+  },
+  bottomBlock: {
+    flex: 1
+  },
+  button: {
+    flex: 1,
+    backgroundColor: '#48BBEC',
+    borderRadius: 8,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    margin: 10
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    alignSelf: 'center'
   }
 });
 
